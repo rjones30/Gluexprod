85a480dee05b4f69092bde9ac080144c4ae8d4dc
Richard Jones <rjones30@gmail.com>
2023-02-14 14:34:07 -0500

    * add instructions for updating the local singularity sandbox image *[rtj]

--- rebuild.sh
+++ rebuild.sh
@@ -1,11 +1,12 @@
 tag=docker.io/rjones30/gluex:latest
 docker build --network host --tag $tag .
 #docker build --no-cache --network host --tag $tag .
-singularity build --sandbox out docker://registry.hub.docker.com/rjones30/gluex:latest
 if [[ $? == 0 ]]; then
     echo -n "docker build --tag $tag completed successfully, to update the image "
     echo "on dockerhub you need to follow up with the command below."
     echo "$ sudo docker push $tag"
+    echo "Then if you want to push the update into a local singularity sandbox, do"
+    echo "$ sudo rm -rf out && singularity build --sandbox out docker://registry.hub.docker.com/rjones30/gluex:latest
 else
     echo -n "docker build --tag $tag failed, please fix the problems before you "
     echo "push the updates to dockerhub."
