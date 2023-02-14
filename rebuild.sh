28fcc1ea19c2c6eca71fb8b55bada4a924672533
Richard T. Jones <richard.t.jones@uconn.edu>
2023-02-14 14:36:36 -0500

    * fix a typo in rebuild.sh [rtj]

--- rebuild.sh
+++ rebuild.sh
@@ -6,7 +6,7 @@ if [[ $? == 0 ]]; then
     echo "on dockerhub you need to follow up with the command below."
     echo "$ sudo docker push $tag"
     echo "Then if you want to push the update into a local singularity sandbox, do"
-    echo "$ sudo rm -rf out && singularity build --sandbox out docker://registry.hub.docker.com/rjones30/gluex:latest
+    echo "$ sudo rm -rf out && singularity build --sandbox out docker://registry.hub.docker.com/rjones30/gluex:latest"
 else
     echo -n "docker build --tag $tag failed, please fix the problems before you "
     echo "push the updates to dockerhub."
