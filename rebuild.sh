4a214582f1dce33b07ff638b9058ee78dc9443af
Richard T. Jones <richard.t.jones@uconn.edu>
2023-02-14 12:53:34 -0500

    * add a singularity build test to the rebuild.sh script [rtj]

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,7 @@
 tag=docker.io/rjones30/gluex:latest
-#docker build --network host --tag $tag .
-docker build --no-cache --network host --tag $tag .
+docker build --network host --tag $tag .
+#docker build --no-cache --network host --tag $tag .
+singularity build --sandbox out docker://registry.hub.docker.com/rjones30/gluex:latest
 if [[ $? == 0 ]]; then
     echo -n "docker build --tag $tag completed successfully, to update the image "
     echo "on dockerhub you need to follow up with the command below."
